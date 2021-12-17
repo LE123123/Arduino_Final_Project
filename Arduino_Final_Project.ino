1702f37e06bb9e5803b5b321077e88239b25543f
LE123123 <heart20021010@gmail.com>
2021-12-17 18:30:25 +0900

    foutrh commit

--- Arduino_Final_Project.ino
+++ Arduino_Final_Project.ino
@@ -1,8 +1,13 @@
 #include<Servo.h>
+#include <LiquidCrystal.h>
 Servo myservo;
 int pos = 0;
 int angle = 0;
 int value1, value2, value3, value4;
+int countRed = 0;
+int countBlue = 0;
+
+int count = 0;
 
 void setup()
 {
@@ -19,6 +24,10 @@ void setup()
   //기본 설정을 해 줍니다.
 
   pinMode(12, OUTPUT);
+
+  pinMode(4, OUTPUT);
+  pinMode(13, OUTPUT);
+
 }
 
 void loop() {
@@ -46,9 +55,9 @@ void ultraSonicAll() {
   value3 = ultraSonic(11, 7);
   value4 = ultraSonic(3, 6);
   
-  if(value1 <= 10 || value2 <= 10 || value3 <= 10 || value4 <= 10) {
-//    Serial.println("Sound Buzzer");
+  if(value1 <= 10 || value2 <= 10 || value3 <= 10 || value4 <= 10) { 
     tone(12, 261, 100);
+    turnLed();
   }
   
   Serial.print(ultraSonic(2, 5));
@@ -79,4 +88,26 @@ int ultraSonic(int trigPin, int echoPin) {
   }
   return distance;
  //초음파 센서값을 측정합니다.
+}
+
+void turnLed() {
+  // 빨간 불을 킨다 ( 빨간불 차례여야 하고 { count가 짝수여야 한다 }  파란불이 꺼져있다 )
+  if(count % 2 == 0 && countBlue == 0) {
+    countRed = 1;
+    digitalWrite(4, HIGH);
+    Serial.println("Red");
+    delay(500);
+    digitalWrite(4, LOW);
+    countRed = 0;
+    count++;
+  // 파란 불을 킨다 ( 파란불 차례여야 { count가 홀수여야 한다 } 하고 빨간불이 꺼져있다면 )
+  } else if(count % 2 != 0 && countRed == 0) {
+    countBlue = 1;  
+    digitalWrite(13, HIGH);
+    Serial.println("Blue");
+    delay(500);
+    digitalWrite(13, LOW);
+    countBlue = 0;
+    count++;
+  }
 }
